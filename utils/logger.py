372c0e58bf19c9be236f756780b680d787cd3e0b
Mikrovatt <viktorov.sergey@inbox.ru>
2023-03-30 18:08:43 +0300

    Repair logger

--- utils/logger.py
+++ utils/logger.py
@@ -33,7 +33,7 @@ class Logger:
         data_to_add = f"Response code: {result.status_code}\n"
         data_to_add += f"Response text: {result.text}\n"
         data_to_add += f"Response headers: {headers_as_dict}\n"
-        data_to_add += f"Response headers: {cookies_as_dict}\n"
+        data_to_add += f"Response cookies: {cookies_as_dict}\n"
         data_to_add += f'\n-----\n'
 
         cls.write_log_to_file(data_to_add)
